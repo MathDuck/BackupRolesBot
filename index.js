92d72909a744eecf29235e5e881c1fc9fa8449ec
MathDuck <thimath62@live.fr>
2021-04-07 19:11:25 +0200

    Added discord.js & Initialising client

---
+++ index.js
@@ -0,0 +1,20 @@
+const { AkairoClient } = require('discord-akairo');
+require("dotenv").config();
+
+class DiscordClient extends AkairoClient {
+    constructor() {
+        super({
+            // Options for Akairo go here.
+        }, {
+            disableEveryone: true,
+            partials: ["MESSAGE", "CHANNEL", "REACTION"],
+              presence: { 
+                activity: { name: "botActivityStatus", type: "WATCHING" },                
+                status: "online"
+              }
+        });
+    }
+}
+
+const client = new DiscordClient();
+client.login(process.env.BOT_TOKEN);
