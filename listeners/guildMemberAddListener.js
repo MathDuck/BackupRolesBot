dac02a326832373e0abf934f33a3ffbcb002d995
MathDuck <thimath62@live.fr>
2021-05-19 04:32:00 +0200

    Fixed log message placement

--- listeners/guildMemberAddListener.js
+++ listeners/guildMemberAddListener.js
@@ -6,7 +6,7 @@ class GuildMemberAddListener extends Listener {
   constructor() {
     super("guildMemberAdd", {
       emitter: "client",
-      event: "guildMemberAdd",
+      event: "guildMemberAdd"
     });
   }
 
@@ -17,24 +17,25 @@ class GuildMemberAddListener extends Listener {
       .all(member.guild.id, member.id);
 
     if (data) {
-      data.forEach((role) => {
+      data.forEach(role => {
         const roleExists = member.guild.roles.cache.get(role.role_id);
         if (roleExists) {
           try {
             member.roles.add(role.role_id);
           } catch (err) {
-            console.log("Error - Probably Permission Error");
+            console.log("Error - Probably Permission");
           }
         }
         guildRoleQueryFactory
           .removeRolestoDatabaseQuery()
           .run(member.guild.id, member.id, role.role_id);
+
+        console.log(
+          `[${dateFormat.getDate(Date.now())}] Rôles de ${
+            member.user.tag
+          } restaurés.`
+        );
       });
-      console.log(
-        `[${dateFormat.getDate(Date.now())}] Rôles de ${
-          member.user.tag
-        } restaurés.`
-      );
     }
   }
 }
